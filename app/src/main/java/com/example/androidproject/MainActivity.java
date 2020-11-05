419d603e31f4ef201bce156236a360570ec5e1b1
Sidharth Bajaj <w0744581@myscc.ca>
2020-11-05 03:36:40 -0500

    navigation drawer working

--- app/src/main/java/com/example/androidproject/MainActivity.java
+++ app/src/main/java/com/example/androidproject/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                R.id.nav_welcome, R.id.nav_contact, R.id.nav_hook,R.id.nav_tips,R.id.nav_process)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
